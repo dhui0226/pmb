46ac345d4ac6601996ea65fe5a4ae0c4eac9c7e6
dhui <dhui0226@gmail.com>
2021-12-30 01:19:06 -0600

    project cards are editable

--- src/components/Projects/Cards.js
+++ src/components/Projects/Cards.js
@@ -1,15 +1,25 @@
+import { useState } from 'react'
 import Card from 'react-bootstrap/Card'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 import './Todo.css'
 
 //map over all cards for adding/deleting cards
 
 const Cards = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div>
-            <Card style={{ cursor: "pointer" }}>
+            <Card 
+                style={{ cursor: "pointer" }}
+                onClick={handleShow}
+            >
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
                   <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of
                     the card's content.
@@ -18,6 +28,21 @@ const Cards = () => {
                   <Card.Link href="#">Another Link</Card.Link>
                 </Card.Body>
             </Card>
+
+            <Modal show={show} onHide={handleClose} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
         </div>
     )
 }
