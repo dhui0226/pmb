fac523d09e693c8f228309e1c8f8f761bb4e8e08
dhui <dhui0226@gmail.com>
2022-02-09 19:29:45 -0600

    build folder

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
+const path = require('path');
 const express = require('express')
 const server = express()
-const port = 4000
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 4000
 const { client } = require('./db')
 const apiRouter = require('./api')
 const cors = require('cors')
 const morgan = require('morgan')
 
 client.connect()
+
+server.use(express.static(publicPath));
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 
 server.use(cors())
 server.use(express.json())
