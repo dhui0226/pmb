db777ac5c3e867639d16016ffe6aa09f3bce7258
dhui <dhui0226@gmail.com>
2022-02-10 00:14:53 -0600

    deploy attempt

--- server/db/client.js
+++ server/db/client.js
@@ -1,11 +1,18 @@
 const { Client } = require('pg')
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pms'
-let config = { connectionString }
+const client = new Client({
+    connectionString,
+    ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : undefined,
+})
+
+module.exports = client
+
+/*let config = { connectionString }
 
 const client = new Client({config})
 
 if (process.env.DATABASE_URL) {
     config.ssl = { rejectUnauthorized: false }
-}
-
-module.exports = client
+}*/ 
