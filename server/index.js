60fd386cc5953a1cfb84e9636c3c24013cfc8653
dhui <dhui0226@gmail.com>
2022-02-10 01:06:20 -0600

    try again

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ client.connect()
 server.use(express.static(publicPath));
 
 server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.js'));
 });
 
 /*server.get('/', (req, res) => {
